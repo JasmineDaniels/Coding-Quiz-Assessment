d1be44e98a2909ef0ecf3552d30e24fce5e8662c
Jasmine Daniels <create.jasminedaniels@gmail.com>
2022-05-22 23:13:16 -0400

    Start Button Activation --js

---
+++ assets/script.js
@@ -0,0 +1,31 @@
+// Get all required elements
+var startBtn = document.querySelector('.start_btn button');
+var infoBox = document.querySelector('.info_box');
+var exitBtn = infoBox.querySelector('.buttons .quit');
+var continueBtn = infoBox.querySelector('.buttons .restart');
+var quizBox = infoBox.querySelector('.quiz_box');
+
+// var scoreResult = document.getElementsByClassName('scoreResult');
+// var latestScore = scoreResult.val().stringfy() + saveScore.val().stringify
+
+
+//If Start Quiz Button is clicked, 
+startBtn.onclick = ()=>{
+    infoBox.classList.add("activeInfo"); //show the info box
+}
+
+//If Exit Button is clicked, 
+exitBtn.onclick = ()=>{
+    infoBox.classList.remove("activeInfo"); // hide info box
+}
+
+//If Continue Button is clicked,
+continueBtn.onclick = ()=>{
+    infoBox.classList.remove("activeInfo"); 
+    quizBox.classList.remove("activeQuiz"); 
+}
+
+
+//If continue => Function Play Game() + Loop 
+// while Play Game, if input value was incorrect score--, 
+// while Play Game, if input value was incorrect time_sec--, 
